53360003c34becbbdbb483ddcf876ed10d758e2e
saikiran-211 <118240747+saikiran-211@users.noreply.github.com>
2022-11-15 10:22:39 +0530

    Create small.c

---
+++ small.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ int main()
+ {
+     int a,b;
+     scanf("%d%d",&a,&b);
+     if(a<b)
+     {
+         printf("a is small");
+     }
+     else
+     {
+         printf("b is small");
+     }
+ }
